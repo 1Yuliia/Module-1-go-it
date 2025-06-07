901a26d5ea9c02805bad4f6a5e7beb343ac860da
1Yuliia <yuliiachunikhina1995@gmail.com>
2025-06-07 15:32:49 +0300

    practice.py

---
+++ practice.py
@@ -0,0 +1,12 @@
+
+#hello 
+# First step: Enter budget
+weekly_budget = input("Enter budget for the week :")
+print(weekly_budget)
+#Ctrl /
+# COMMAND /
+#weekly_budget=int(input (weekly_budget)
+print(weekly_budget = 100)
+#Ctrl /
+# COMMAND /
+# '300A'
